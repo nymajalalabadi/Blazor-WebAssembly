77a095e55804bfabfecc29b91f479ceee183da1a
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-05-29 12:22:17 +0330

    Added Shoping.Cart FA-IR

--- Blazor WebAssembly/Program.cs
+++ Blazor WebAssembly/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyBlazor.Libraries.Product;
 using MyBlazor.Libraries.ShoppingCart;
 using MyBlazor.Libraries.Storage;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -26,12 +27,12 @@ builder.Configuration.AddJsonStream(stream);
 #region Production
 
 using var responseP = await httpClient.GetAsync("ProductSettings."
-    +
-    builder.HostEnvironment.IsEnvironment
-    +
-    ".json");
+                                                +
+                                                builder.HostEnvironment.Environment
+                                                +
+                                                ".json");
 
-if (response.IsSuccessStatusCode)
+if (responseP.IsSuccessStatusCode)
 {
     using var streamP = await responseP.Content.ReadAsStreamAsync();
 
@@ -43,5 +44,10 @@ if (response.IsSuccessStatusCode)
 builder.Services.AddSingleton<IStorageService, StorageService>();
 builder.Services.AddTransient<IProductService, ProductService>();
 builder.Services.AddTransient<IShoppingCartService, ShoppingCartService>();
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("fa-IR");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("fa-IR");
+
+builder.Services.AddLocalization();
 
 await builder.Build().RunAsync();
